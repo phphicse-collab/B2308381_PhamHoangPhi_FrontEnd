bf4ee6c83d306a05acda4378b09fc67ad715be35
HoangPhi_CSE <phphi.cse@gmail.com>
2026-03-15 01:35:26 +0700

    Tao trang them lien he

--- src/router/index.js
+++ src/router/index.js
@@ -18,6 +18,11 @@ const routes = [
     name: "notfound",
     component: () => import("@/views/NotFound.vue"),
   },
+  {
+        path: "/contacts/add",
+        name: "contact.add",
+        component: () => import("@/views/ContactAdd.vue"),
+  },
 ];
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
